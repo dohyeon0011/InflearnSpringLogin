57ff8d7dee62bdfe784f041520bb90589b569570
idohyeon <ehgus5026@naver.com>
2024-05-22 22:22:00 +0900

    -Session intergration Id

---
+++ src/main/java/hello/login/web/SessionConst.java
@@ -0,0 +1,9 @@
+package hello.login.web;
+
+// 얘는 앞으로 생성할 객체도 아니고, 글자만 참고해서 쓰려는 거라 생성되지 않게
+// 추상 클래스나 인터페이스로 생성
+public abstract class SessionConst {
+
+    public static final String LOGIN_MEMBER = "loginMember";
+
+}
